87ebe6434943d618ce66576d4ba14c8c2392edfa
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-04-09 17:07:07 -0300

    feat: yup config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import { router } from './routes';
 const server = express();
 
+server.use(express.json());
 server.use(router);
 
 export { server };
---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import * as yup from 'yup';
+
+interface ICidade {
+  nome: string;
+}
+
+const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errrors: {
+        default: yupError.message,
+      },
+    });
+  }
+
+  console.log(validateData);
+
+  return res.send('Create');
+};
