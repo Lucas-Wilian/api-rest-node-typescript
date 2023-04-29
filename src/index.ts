2e314664604738a98fb0f57577ec141b48262df1
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-04-29 15:30:40 -0300

    feat: bugs production

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { server } from './server/Server';
+import { Knex } from './server/database/knex';
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log(`Servidor no ar na porta ${process.env.PORT || 3333} 🚀🔥 `)
-);
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () =>
+    console.log(`Servidor no ar na porta ${process.env.PORT || 3333} 🚀🔥 `)
+  );
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate
+    .latest()
+
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
