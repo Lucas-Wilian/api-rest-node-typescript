157f2b27641b0fc05cdaa12ee14e7cca4a9d6153
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-05-02 22:18:19 -0300

    feat: bug

--- src/server/database/providers/pessoas/Create.ts
+++ src/server/database/providers/pessoas/Create.ts
@@ -1,19 +1,19 @@
 import { EtableNames } from '../../EtableName';
-import { Knex } from '../../knex';
 import { IPessoa } from '../../models';
+import { Knex } from '../../knex';
 
 export const create = async (
   pessoa: Omit<IPessoa, 'id'>
 ): Promise<number | Error> => {
-  const [{ count }] = await Knex(EtableNames.pessoa)
-    .where('id', '=', pessoa.cidadeId)
-    .count<[{ count: number }]>('* as count');
-
-  if (count === 0) {
-    return new Error('A cidade usada no cadastro não foi encontrada');
-  }
-
   try {
+    const [{ count }] = await Knex(EtableNames.cidade)
+      .where('id', '=', pessoa.cidadeId)
+      .count<[{ count: number }]>('* as count');
+
+    if (count === 0) {
+      return new Error('A cidade usada no cadastro não foi encontrada');
+    }
+
     const [result] = await Knex(EtableNames.pessoa)
       .insert(pessoa)
       .returning('id');
@@ -22,6 +22,7 @@ export const create = async (
     } else if (typeof result === 'number') {
       return result;
     }
+
     return new Error('Erro ao cadastrar o registro');
   } catch (error) {
     console.log(error);
