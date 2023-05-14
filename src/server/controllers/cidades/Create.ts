cd41140598492f3c3d4fa7ba6d27c33a4556756c
Lucas-Wilian <lucaswilianr.s@gmail.com>
2023-05-14 20:00:06 -0300

    improved query.id

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,32 +1,51 @@
-import { Request, RequestHandler, Response } from 'express';
-import * as yup from 'yup';
-import { validation } from '../../shared/middleware';
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { ICidade } from '../../database/models';
+import * as yup from 'yup';
+
 import { CidadesProvider } from '../../database/providers/cidades';
+import { validation } from '../../shared/middleware';
 
-interface IBodyProps extends Omit<ICidade, 'id'> {}
-
-export const createValidation = validation((getSchema) => ({
-  body: getSchema<IBodyProps>(
+interface IQueryProps {
+  id?: number;
+  page?: number;
+  limit?: number;
+  filter?: string;
+}
+export const getAllValidation = validation((getSchema) => ({
+  query: getSchema<IQueryProps>(
     yup.object().shape({
-      nome: yup.string().required().min(3).max(150),
+      page: yup.number().moreThan(0),
+      limit: yup.number().moreThan(0),
+      id: yup.number().integer().default(0),
+      filter: yup.string(),
     })
   ),
 }));
 
-export const create: RequestHandler = async (
-  req: Request<{}, {}, IBodyProps>,
+export const getAll = async (
+  req: Request<{}, {}, {}, IQueryProps>,
   res: Response
 ) => {
-  const result = await CidadesProvider.create(req.body);
+  const result = await CidadesProvider.getAll(
+    req.query.page || 1,
+    req.query.limit || 7,
+    req.query.filter || '',
+    Number(req.query.id || 0)
+  );
+  const count = await CidadesProvider.count(req.query.filter);
+
   if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: {
-        default: result.message,
-      },
+      errors: { default: result.message },
+    });
+  } else if (count instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: count.message },
     });
   }
 
-  return res.status(StatusCodes.CREATED).json(result);
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', count);
+
+  return res.status(StatusCodes.OK).json(result);
 };
